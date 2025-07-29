dbe37f924f89a7a81d82234ffd2e7e5ae50b455d
sam09040 <jpalmeida0904@gmail.com>
2025-07-29 20:05:23 -0300

    Adicionando Services para o usuário

---
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package me.dio.service.impl;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.stereotype.Service;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+import me.dio.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User createUser(User userToCreate) {
+        if (userRepository.existsById(userToCreate.getId()) && userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This user already exists!");
+        }
+        return userRepository.save(userToCreate);
+    }
+    
+}
